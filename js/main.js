3b62786d2926ad71ec06d7095d80627399393c35
DhakalRupesh <a73dhakal@outlook.com>
2020-12-02 13:54:03 +0545

    reveal animation

--- js/main.js
+++ js/main.js
@@ -6,17 +6,17 @@ window.addEventListener('scroll', function(){
 });
 
 // redirecting pages
-function visitBreakfast() {
-    window.location.href = "pages/breakfast.html";
-}
+// function visitBreakfast() {
+//     window.location.href = "pages/breakfast.html";
+// }
 
-function visitLunch() {
-    window.location.href = "pages/lunch.html";
-}
+// function visitLunch() {
+//     window.location.href = "pages/lunch.html";
+// }
 
-function visitDinner() {
-    window.location.href = "pages/dinner.html";
-}
+// function visitDinner() {
+//     window.location.href = "pages/dinner.html";
+// }
 
 // changing tabs
 function openFood(foodName) {
@@ -31,30 +31,12 @@ function openFood(foodName) {
 /*===== SCROLL REVEAL ANIMATION =====*/
 const sr = ScrollReveal({
     origin: 'top',
-    distance: '80px',
-    duration: 2000,
-    reset: true
+    distance: '30px',
+    duration: 1500,
+    reset: false
 });
 
-/*SCROLL HOME*/
-sr.reveal('.home__title',{}); 
-sr.reveal('.button',{delay: 200}); 
-sr.reveal('.home__img',{delay: 400}); 
-sr.reveal('.home__social-icon',{ interval: 200}); 
+sr.reveal('.brand',{delay: 100}); 
+sr.reveal('.nav_buttons',{delay: 100}); 
+sr.reveal('.body_container',{delay: 100}); 
 
-/*SCROLL ABOUT*/
-sr.reveal('.about__img',{}); 
-sr.reveal('.about__subtitle',{delay: 400}); 
-sr.reveal('.about__text',{delay: 400}); 
-
-/*SCROLL SKILLS*/
-sr.reveal('.skills__subtitle',{}); 
-sr.reveal('.skills__text',{}); 
-sr.reveal('.skills__data',{interval: 200}); 
-sr.reveal('.skills__img',{delay: 600});
-
-/*SCROLL WORK*/
-sr.reveal('.work__img',{interval: 200}); 
-
-/*SCROLL CONTACT*/
-sr.reveal('.contact__input',{interval: 200}); 
